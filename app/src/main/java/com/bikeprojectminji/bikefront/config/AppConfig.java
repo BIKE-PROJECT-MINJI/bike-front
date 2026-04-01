7ba179247d5e2717c1e8a4a0c958289132ce8f1e
minji0828 <entersuk1998@naver.com>
2026-04-02 03:07:33 +0900

    feat: ride 지도와 경로 표시 추가

--- app/src/main/java/com/bikeprojectminji/bikefront/config/AppConfig.java
+++ app/src/main/java/com/bikeprojectminji/bikefront/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.bikeprojectminji.bikefront.config;
 public final class AppConfig {
 
     public static final String API_BASE_URL = "http://10.0.2.2:8080";
+    public static final String MAP_STYLE_URL = "https://demotiles.maplibre.org/style.json";
     public static final int CONNECT_TIMEOUT_MS = 5000;
     public static final int READ_TIMEOUT_MS = 5000;
 
