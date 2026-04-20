2a631439bc84f2e7bf3b89b55cbea67126f9bcb7
MINJI0828 <MINJI0828@users.noreply.github.com>
2026-04-20 11:19:21 +0900

    feat: 추천 코스 응답 모델을 Compose 홈 흐름에 맞게 정리

--- app/src/main/java/com/bikeprojectminji/bikefront/home/FeaturedCourseRepository.java
+++ app/src/main/java/com/bikeprojectminji/bikefront/home/FeaturedCourseRepository.java
@@ -1,14 +1,5 @@
 package com.bikeprojectminji.bikefront.home;
 
-public class FeaturedCourseRepository {
-
-    private final FeaturedCourseApiClient apiClient;
-
-    public FeaturedCourseRepository(FeaturedCourseApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
-
-    public FeaturedCourseResponse loadFeaturedCourses() throws Exception {
-        return apiClient.fetchFeaturedCourses();
-    }
+public interface FeaturedCourseRepository {
+    FeaturedCourseResponse loadFeaturedCourses() throws Exception;
 }
